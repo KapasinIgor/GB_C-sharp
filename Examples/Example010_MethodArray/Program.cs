dac98aea67bb02240071a6e65740647f50afff04
Kapasin <kapasin.igor@gmail.com>
2022-09-18 12:32:06 +0300

    add Example010_MethodArray

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int[] array = {12, 4, 45, 56, 25, 26, 36, 7, 16};
+
+int index = 0;
+int find = 4;
+
+while (index < array.Length)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
