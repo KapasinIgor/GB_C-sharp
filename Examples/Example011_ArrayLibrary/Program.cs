9ac11c2518b8acd5edb91c1b7538ca90dfdab83b
Kapasin <kapasin.igor@gmail.com>
2022-09-18 12:59:51 +0300

    add Example011_ArrayLibrary

---
+++ Examples/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+void FillArray(int[] collection)
+{
+    int index = 0;
+    while (index < collection.Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] collect)
+{
+    int position = 0;
+    while (position < collect.Length)
+    {
+        Console.Write($"{collect[position]} ");
+        position++;
+    }
+}
+
+int FindIndexOf(int[] collection, int find)
+{
+    int index = 0;
+    int posit = -1;
+    while (index < collection.Length)
+    {
+        if (collection[index] == find)
+        {
+            posit = index;
+        }
+        index++;
+    }
+    return posit;
+}
+
+int[] array = new int[10]; 
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = FindIndexOf(array, 4);
+if (pos == -1) Console.WriteLine("элемент отсутствует");
+else Console.WriteLine(pos);
