848b0cca99a36e4a2f0d7812b503ccec05ce19b4
Kapasin <kapasin.igor@gmail.com>
2022-10-02 19:24:59 +0300

    add Seminar006_RecursionsAndTwoDimensionalArrays

---
+++ Tasks/Seminar006_RecursionsAndTwoDimensionalArrays/task41/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int ZeroOf(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sum++;
+    }
+    return sum;
+}
+
+Console.Write("Введите количество чисел в массиве: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[count]; 
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int sum = ZeroOf(array);
+Console.Write(sum); 
+
---
+++ Tasks/Seminar006_RecursionsAndTwoDimensionalArrays/task43/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2 && b1 == b2) Console.Write("Прямые равны друг другу - бесконечно много точек");
+else if (k1 == k2) Console.Write("Прямые параллельны - никогда не пересекутся");
+else 
+{
+    double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+    double y = Math.Round(k1 * x + b1, 2);
+    Console.Write($"({x};{y})");
+}
