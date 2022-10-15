8f37cff69468599c40ab4e8dda6197a6458d2fd3
Kapasin <kapasin.igor@gmail.com>
2022-10-15 22:08:46 +0300

    add Seminar009_Recursions

---
+++ Tasks/Seminar009_Recursions/task66/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int SumElements(int M, int N)
+{
+    if (M == N) return M;
+    return SumElements(M, N - 1) + N;
+}
+
+Console.Write("Введите начальное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Сумма чисел от {M} до {N} равна {SumElements(M, N)}");
---
+++ Tasks/Seminar009_Recursions/task68/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int Ackerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Ackerman(m - 1, 1);
+    else return Ackerman(m - 1, Ackerman(m, n - 1));
+}
+
+Console.Write("Введите начальное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Вычисление по функции Аккермана - {Ackerman(m, n)}");
